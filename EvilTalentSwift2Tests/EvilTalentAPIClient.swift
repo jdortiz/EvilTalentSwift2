c80464fbf2b55bfa67ac1cd3a3f52c1f4fa23869
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-08-18 01:13:21 -0700

    Add first test for when JSON parsing fails.
    The completion closure that would be invoked with the parsed data shouldn't be invoked.

--- EvilTalentSwift2Tests/EvilTalentAPIClient.swift
+++ EvilTalentSwift2Tests/EvilTalentAPIClient.swift
@@ -12,19 +12,38 @@ import XCTest
 
 
 class EvilTalentAPIClientTests: XCTestCase {
+    var sut: EvilTalentAPIClient!
+    var completionInvoked = false
     
+
     // MARK: - Set Up & Tear
     
     override func setUp() {
         super.setUp()
+        sut = EvilTalentAPIClient()
     }
     
     override func tearDown() {
+        sut = nil
         super.tearDown()
     }
     
     
     // MARK: - Tests
     
+    func testParseServerDataDoesntCallCompletionWithBrokenJSON() {
+        let brokenJsonData = "{".dataUsingEncoding(NSUTF8StringEncoding)
+        sut.completion = fakeCompletion
+        
+        sut.parseServerData(brokenJsonData, response: nil, error: nil)
+        
+        XCTAssertFalse(completionInvoked, "Completion closure must not be called with broken JSON data.")
+    }    
     
+    
+    // MARK: - Auxiliary methods
+    
+    func fakeCompletion(jsonData: [NSDictionary]) -> Void {
+        completionInvoked = true
+    }
 }
