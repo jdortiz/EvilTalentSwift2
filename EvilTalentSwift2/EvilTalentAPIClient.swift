571df49ede413584afebb2e84a09c72b0d710ce6
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-08-18 01:03:12 -0700

    Refactor EvilTalentAPIClient so the code is more testable.

--- EvilTalentSwift2/EvilTalentAPIClient.swift
+++ EvilTalentSwift2/EvilTalentAPIClient.swift
@@ -12,24 +12,33 @@ import Foundation
 
 class EvilTalentAPIClient {
     let endPoint = "https://api.eviltalent.nowhere/v1/evildudes"
+
     
+    // MARK: - Properties
+    
+    var completion: ((jsonData: [NSDictionary]) -> Void)?
+
+
     func fetchEvilDudes(completion: (jsonData: [NSDictionary]) -> Void) {
         guard let URL = NSURL(string: endPoint) else {
             return
         }
-        let task = NSURLSession.sharedSession().dataTaskWithURL(URL) { (data, response, error) -> Void in
-            guard error == nil && data != nil  else {
-                print("Failed to download data from the site.", appendNewline: true)
-                return
-            }
-            do {
-                if case let dictionaries as [NSDictionary] = try NSJSONSerialization.JSONObjectWithData(data!, options: []) {
-                    completion(jsonData: dictionaries)
-                }
-            } catch {
-                print("Unexpected data format provided by server.", appendNewLine: true)
-            }
-        }
+        let task = NSURLSession.sharedSession().dataTaskWithURL(URL, completionHandler: parseServerData)
         task.resume()
+    }
+
+    
+    func parseServerData(data: NSData?, response: NSURLResponse?, error: NSError?) {
+        guard error == nil && data != nil  else {
+            print("Failed to download data from the site.", appendNewline: true)
+            return
+        }
+        do {
+            if case let dictionaries as [NSDictionary] = try NSJSONSerialization.JSONObjectWithData(data!, options: []) {
+                completion?(jsonData: dictionaries)
+            }
+        } catch {
+            print("Unexpected data format provided by server.", appendNewLine: true)
+        }
     }
 }
