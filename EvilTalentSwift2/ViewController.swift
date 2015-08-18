a80a29e340211d43c361be3033a811583baa89c9
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-08-18 00:23:17 -0700

    Initial Commit

---
+++ EvilTalentSwift2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EvilTalentSwift2
+//
+//  Created by Jorge D. Ortiz Fuentes on 18/8/15.
+//  Copyright © 2015 powwau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ EvilTalentSwift2Tests/EvilTalentSwift2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  EvilTalentSwift2Tests.swift
+//  EvilTalentSwift2Tests
+//
+//  Created by Jorge D. Ortiz Fuentes on 18/8/15.
+//  Copyright © 2015 powwau. All rights reserved.
+//
+
+import XCTest
+@testable import EvilTalentSwift2
+
+class EvilTalentSwift2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
