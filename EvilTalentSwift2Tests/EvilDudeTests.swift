aa0b633b33adb48b43a3bd076b65d5e42926957d
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-08-18 00:38:48 -0700

    Finish Demo 1.
    Add tests for the EvilDude class: initialization, stored properties, and computed property for rating.

--- EvilTalentSwift2Tests/EvilDudeTests.swift
+++ EvilTalentSwift2Tests/EvilDudeTests.swift
@@ -11,14 +11,29 @@ import XCTest
 
 
 class EvilDudeTests: XCTestCase {
+    // MARK: - Constants & parameters
+    static let mainName = "Juanita Banana"
+    static let mainDestructivePower = 4
+    static let mainMotivation = 2
+    static let mainRating = 3
     
+    
+    // MARK: - Properties
+    var sut: EvilDude!
+    let jsonData = [ EvilDude.dictionaryKeyName: "Juanita Banana",
+        EvilDude.dictionaryKeyDestructivePower: 4,
+        EvilDude.dictionaryKeyMotivation: 2 ]
+    
+
     // MARK: - Set Up & Tear
     
     override func setUp() {
         super.setUp()
+        sut = EvilDude(dictionary: jsonData)
     }
     
     override func tearDown() {
+        sut = nil
         super.tearDown()
     }
     
@@ -27,5 +42,34 @@ class EvilDudeTests: XCTestCase {
     
     func testEvilDudeIsntInitialicedFromEmptyDict() {
         XCTAssertNil(EvilDude(dictionary: NSDictionary()), "No object must be returned from an empty dictionary")
+    }
+    
+    
+    func testEvilDudeIsInitialicedFromProperDict() {
+        XCTAssertNotNil(sut, "An EvilDude object must be returned from a dictionary with proper data.")
+    }
+    
+    
+    func testEvilDudeNameAssignement() {
+        XCTAssertEqual(sut.name, EvilDudeTests.mainName,
+            "Name should be assigned to the value in the dictionary.")
+    }
+    
+    
+    func testEvilDudeDestructivePowerAssignement() {
+        XCTAssertEqual(sut.destructivePower, EvilDudeTests.mainDestructivePower,
+            "Destructive power should be assigned to the value in the dictionary.")
+    }
+    
+    
+    func testEvilDudeMotivationAssignement() {
+        XCTAssertEqual(sut.motivation, EvilDudeTests.mainMotivation,
+            "Motivation should be assigned to the value in the dictionary.")
+    }
+    
+    
+    func testEvilDudeRatingComputation() {
+        XCTAssertEqual(sut.rating, EvilDudeTests.mainRating,
+            "Rating should be computed with the destructive power and the motivation.")
     }
 }
